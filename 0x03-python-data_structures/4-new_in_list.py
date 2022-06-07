9b9be3dbe5b9bfe1bdb69e82dca488f9a9c1db36
sirobembs <danielobembe1@gmail.com>
2022-06-06 19:50:51 -0700

    unction that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,8 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
 
     if idx < 0 or idx > length:
-        new_list[idx] = element
+         new_list[idx] = element
+        return(new_list)
+
 
     return (new_list)
