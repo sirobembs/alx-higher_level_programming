462a5a26e66175710ad47dda72ba0b69316d6887
sirobembs <danielobembe1@gmail.com>
2022-07-04 21:26:35 -0700

    a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class based geometry with public instnce
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
