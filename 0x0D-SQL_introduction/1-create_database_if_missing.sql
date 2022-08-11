0fa31f2140a0105f1c8940622b2237a1a166e45a
Daniel Obembe <danielobembe1@gmail.com>
2022-08-11 20:46:27 +0100

    script that creates the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- 1-create_database_if_missing.sql
+-- script that creates a database in MySQL server
+
+CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0
+[CHARACTER SET charset_name]
+[COLLATE collation_name]
