57c653c0445a5a15d4c89453d323ffa496b0d941
Daniel Obembe <danielobembe1@gmail.com>
2022-08-12 05:50:37 +0100

    script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,6 @@
 -- Script that lists shows by their rating
+-- you can only use one select statement
+
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
 FROM tv_shows
 JOIN tv_show_ratings
