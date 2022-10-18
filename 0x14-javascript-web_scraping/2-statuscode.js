ebfb82caa22cf7537a60ed38846beb629ec26578
Daniel Obembe <danielobembe1@gmail.com>
2022-10-18 08:39:31 +0100

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
+});
